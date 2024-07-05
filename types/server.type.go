bf03c27d83b0e2e19e8bcf86e170ecf7fd9356b6
Ahmed Aladdin <aamieeu@gmail.com>
2024-07-05 23:10:05 +0300

    Create the server struct

--- types/server.type.go
+++ types/server.type.go
@@ -2,10 +2,12 @@ package types
 
 type Server struct {
 	ListenAddr string
+	DB         Storage
 }
 
-func NewServer(listenAddr string) *Server {
+func NewServer(listenAddr string, store Storage) *Server {
 	return &Server{
 		ListenAddr: listenAddr,
+		DB:         store,
 	}
 }
