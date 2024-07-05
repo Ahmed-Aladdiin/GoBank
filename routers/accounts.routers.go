31100484ad3726dd533212fedd6684f86eceb6f7
Ahmed Aladdin <aamieeu@gmail.com>
2024-07-05 23:13:53 +0300

    Route accounts handlers

--- routers/accounts.routers.go
+++ routers/accounts.routers.go
@@ -4,29 +4,30 @@ import (
 	"net/http"
 
 	"github.com/ahmed-aladdiin/gobank/handlers"
+	"github.com/ahmed-aladdiin/gobank/types"
 	"github.com/ahmed-aladdiin/gobank/utils"
 )
 
-func AccountsRouter(w http.ResponseWriter, r *http.Request) {
+func AccountsRouter(w http.ResponseWriter, r *http.Request, server *types.Server) {
 	switch r.Method {
 	case "GET":
-		handlers.HandleGETccounts(w, r)
+		handlers.HandleGETccounts(w, r, server)
 		return
 	case "POST":
-		handlers.HandlePOSTAccount(w, r)
+		handlers.HandlePOSTAccount(w, r, server)
 		return
 	}
 
 	utils.WriteJSON(w, http.StatusBadRequest, utils.ApiError{Error: "Method not supported1"})
 }
 
-func AccountsRouterByID(w http.ResponseWriter, r *http.Request) {
+func AccountsRouterByID(w http.ResponseWriter, r *http.Request, server *types.Server) {
 	switch r.Method {
 	case "GET":
-		handlers.HandleGETccount(w, r)
+		handlers.HandleGETccount(w, r, server)
 		return
 	case "DELETE":
-		handlers.HandleDELETEAccount(w, r)
+		handlers.HandleDELETEAccount(w, r, server)
 		return
 	}
 
