6a878a49da8225e062530d03334a625f24920bca
Ahmed Aladdin <aamieeu@gmail.com>
2024-07-05 23:08:38 +0300

    Setup helping utilities

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,13 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, object any) error {
@@ -14,4 +20,28 @@ func WriteJSON(w http.ResponseWriter, status int, object any) error {
 
 type ApiError struct {
 	Error string `json:"error"`
+}
+
+func GetID(r *http.Request) (int, error) {
+	id_string := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(id_string)
+	if err != nil {
+		return 0, errors.New("Unvalid ID")
+	}
+
+	return id, nil
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
