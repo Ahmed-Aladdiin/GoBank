018b48c8b3598c52400675a91a47767aef7bd01c
Ahmed Aladdin <aamieeu@gmail.com>
2024-07-05 23:13:19 +0300

    Create handlers for accounts manipulations

--- handlers/account.handlers.go
+++ handlers/account.handlers.go
@@ -1,23 +1,64 @@
 package handlers
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
 
 	"github.com/ahmed-aladdiin/gobank/types"
 	"github.com/ahmed-aladdiin/gobank/utils"
 )
 
-func HandleGETccounts(w http.ResponseWriter, r *http.Request) {
+func HandleGETccounts(w http.ResponseWriter, r *http.Request, server *types.Server) {
+	accounts, err := server.DB.GetAccounts()
+	if err != nil {
+		utils.WriteJSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, accounts)
 }
 
-func HandleGETccount(w http.ResponseWriter, r *http.Request) {
-	log.Println("once")
-	utils.WriteJSON(w, http.StatusOK, types.NewAccount("Ahmed", "Aladdin"))
+func HandleGETccount(w http.ResponseWriter, r *http.Request, server *types.Server) {
+	id, err := utils.GetID(r)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	account, err := server.DB.GetAccountByID(id)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusNotFound, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, account)
 }
 
-func HandlePOSTAccount(w http.ResponseWriter, r *http.Request) {
+func HandlePOSTAccount(w http.ResponseWriter, r *http.Request, server *types.Server) {
+	account := new(types.Account)
+	json.NewDecoder(r.Body).Decode(account)
+
+	newAcc, err := server.DB.CreateAccount(account)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, newAcc)
 }
 
-func HandleDELETEAccount(w http.ResponseWriter, r *http.Request) {
+func HandleDELETEAccount(w http.ResponseWriter, r *http.Request, server *types.Server) {
+	id, err := utils.GetID(r)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	err2 := server.DB.DeleteAccount(id)
+	if err2 != nil {
+		utils.WriteJSON(w, http.StatusNotFound, utils.ApiError{Error: err2.Error()})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, nil)
 }
